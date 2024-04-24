954a776630c6fd7d3dfb095b527b375f94452329
Isabel <isabel@isabelroses.com>
2024-04-24 17:50:02 +0100

    feat: add flake (#2)
    Co-authored-by: uncenter <47499684+uncenter@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "sttr-rs - Easily change strings and convert string representations of data";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgsForEach = nixpkgs.legacyPackages;
+  in {
+    packages = forEachSystem (system: {
+      default = pkgsForEach.${system}.callPackage ./nix/default.nix {};
+    });
+
+    devShells = forEachSystem (system: {
+      default = pkgsForEach.${system}.callPackage ./nix/shell.nix {};
+    });
+  };
+}
