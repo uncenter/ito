753bc1a9d280e37e4368543bccc0848b7b0ea474
uncenter <47499684+uncenter@users.noreply.github.com>
2024-04-26 11:49:43 -0400

    feat: add tui/interactive mode

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use clap::{Parser, Subcommand};
 use clap_stdin::MaybeStdin;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+	prelude::*,
+	symbols::border,
+	widgets::{block::*, *},
+};
+use std::io;
 
 use eyre::Result;
 
+mod tui;
 mod lib {
 	pub mod base64;
 	pub mod case;
@@ -12,6 +20,90 @@ mod lib {
 }
 
 use lib::*;
+
+#[derive(Debug, Default)]
+pub struct App {
+	counter: u8,
+	exit: bool,
+}
+
+impl App {
+	pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
+		while !self.exit {
+			terminal.draw(|frame| self.render_frame(frame))?;
+			self.handle_events()?;
+		}
+		Ok(())
+	}
+
+	fn render_frame(&self, frame: &mut Frame) {
+		frame.render_widget(self, frame.size());
+	}
+
+	fn handle_events(&mut self) -> io::Result<()> {
+		match event::read()? {
+			Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+				self.handle_key_event(key_event)
+			}
+			_ => {}
+		};
+		Ok(())
+	}
+
+	fn handle_key_event(&mut self, key_event: KeyEvent) {
+		match key_event.code {
+			KeyCode::Char('q') => self.exit(),
+			KeyCode::Left => self.decrement_counter(),
+			KeyCode::Right => self.increment_counter(),
+			_ => {}
+		}
+	}
+
+	fn exit(&mut self) {
+		self.exit = true;
+	}
+
+	fn increment_counter(&mut self) {
+		self.counter += 1;
+	}
+
+	fn decrement_counter(&mut self) {
+		self.counter -= 1;
+	}
+}
+
+impl Widget for &App {
+	fn render(self, area: Rect, buf: &mut Buffer) {
+		let title = Title::from(" sttr ".bold());
+		let instructions = Title::from(Line::from(vec![
+			" Decrement ".into(),
+			"<Left>".blue().bold(),
+			" Increment ".into(),
+			"<Right>".blue().bold(),
+			" Quit ".into(),
+			"<Q> ".blue().bold(),
+		]));
+		let block = Block::default()
+			.title(title.alignment(Alignment::Center))
+			.title(
+				instructions
+					.alignment(Alignment::Center)
+					.position(Position::Bottom),
+			)
+			.borders(Borders::ALL)
+			.border_set(border::THICK);
+
+		let counter_text = Text::from(vec![Line::from(vec![
+			"Value: ".into(),
+			self.counter.to_string().yellow(),
+		])]);
+
+		Paragraph::new(counter_text)
+			.centered()
+			.block(block)
+			.render(area, buf);
+	}
+}
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -172,6 +264,11 @@ fn main() -> Result<()> {
 				} => misc::replace(text, find, replace),
 			}
 		);
+	} else {
+		let mut terminal = tui::init()?;
+		let app_result = App::default().run(&mut terminal);
+		tui::restore()?;
+		app_result?
 	}
 
 	Ok(())
