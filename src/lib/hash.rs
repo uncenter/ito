635b1a0835242a163e178f416f4f3d429253fd75
Aubrey <cat@aubrey.rs>
2024-07-24 21:22:53 +0100

    rustfmt

--- src/lib/hash.rs
+++ src/lib/hash.rs
@@ -1,31 +1,31 @@
-use clap_stdin::MaybeStdin;
 use bcrypt::{hash, DEFAULT_COST};
+use clap_stdin::MaybeStdin;
 use md5 as md5_crate;
 use sha1::Sha1;
-use sha2::{Sha256, Sha512, Digest};
+use sha2::{Digest, Sha256, Sha512};
 
 pub fn bcrypt(text: MaybeStdin<String>) -> String {
-    hash(text.as_bytes(), DEFAULT_COST).unwrap()
+	hash(text.as_bytes(), DEFAULT_COST).unwrap()
 }
 
 pub fn md5(text: MaybeStdin<String>) -> String {
-    format!("{:x}", md5_crate::compute(text.as_bytes()))
+	format!("{:x}", md5_crate::compute(text.as_bytes()))
 }
 
 pub fn sha1(text: MaybeStdin<String>) -> String {
-    let mut hasher = Sha1::new();
-    hasher.update(text.as_bytes());
-    format!("{:x}", hasher.finalize())
+	let mut hasher = Sha1::new();
+	hasher.update(text.as_bytes());
+	format!("{:x}", hasher.finalize())
 }
 
 pub fn sha256(text: MaybeStdin<String>) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(text.as_bytes());
-    format!("{:x}", hasher.finalize())
+	let mut hasher = Sha256::new();
+	hasher.update(text.as_bytes());
+	format!("{:x}", hasher.finalize())
 }
 
 pub fn sha512(text: MaybeStdin<String>) -> String {
-    let mut hasher = Sha512::new();
-    hasher.update(text.as_bytes());
-    format!("{:x}", hasher.finalize())
+	let mut hasher = Sha512::new();
+	hasher.update(text.as_bytes());
+	format!("{:x}", hasher.finalize())
 }
